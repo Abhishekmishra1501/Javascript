5be0290d51cb647633d12eb6957a7f5ee0774a2d
Abhishek mishra <166721961+Abhishekmishra1501@users.noreply.github.com>
2024-04-30 00:56:48 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId =121
+let accountEmail = "mishraabhishek7960@gmail.com"
+var accountpassword ="1234"
+accountCity = "Gorakhpur"
+let accountState;
+
+// Prefeer not to use var
+//because of issuce in block scope and fuctional scope 
+
+console.log(accountState);
